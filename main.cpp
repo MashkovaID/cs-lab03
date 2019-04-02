2df209adec4ae27e7ad248bac862dc6a7f1e65e4
MashkovaID <u211-12@public.mpei.local>
2019-04-02 15:36:11 +0300

    code:заготовка программы

---
+++ main.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//ввод чисел
+vector<double> input_numbers(size_t number_count)
+{
+    vector<double> result(number_count);
+    for (size_t i=0; i<number_count; i++)
+    {cin>>result[i];}
+    return result;
+}
+
+//оиск минимума и максимума
+void find_minmax(vector<double> numbers, double& min, double& max)
+{
+    min=numbers[0];
+    max=numbers[0];
+    for(double number : numbers)
+    {
+        if(number<min)
+            {min=number;}
+        if(number>max)
+            {max=number;}
+    }
+}
+//расчет количества чисел в столбцах
+vector<size_t> make_histogram(vector<double> numbers, size_t bin_count)
+{
+    double min, max;
+    find_minmax(numbers, min, max);
+    vector<size_t> bins(bin_count);
+    for(double number : numbers)
+    {
+        size_t bin=(size_t)((number-min)/(max-min)*bin_count);
+        if(bin==bin_count)
+            {bin--;}
+    bins[bin]++;
+    }
+    return bins;
+}
+
+//вывод гистограммы
+void show_histogram_text(const vector<size_t>& bins)
+{
+    const size_t MAX=80-4;
+    size_t max_count=0;
+    for(size_t bin : bins)
+    {
+        if(bin>max_count)
+            {max_count=bin;}
+    }
+    const bool scaling_needed=max_count>MAX;
+
+    for (size_t bin : bins)
+    {
+        if(bin<100) {cout<<" ";}
+        if(bin<10) {cout<<"  ";}
+        cout<<bin<<"|";
+
+    size_t height = bin;
+    if(scaling_needed)
+    {
+        const double scaling_factor=(double)MAX/max_count;
+        height=(size_t)(bin*scaling_factor);
+    }
+
+    for(size_t i=0; i<height; i++)
+        {cout<<"*";}
+    cout<<"\n";
+    }
+}
+
+int main()
+{
+    size_t number_count;
+    cerr<<" Enter number count\n";
+    cin>>number_count;
+
+    cerr<<" Enter numbers\n";
+    const auto numbers = input_numbers(number_count);
+
+    size_t bin_count;
+    cerr<<" Enter bin count\n";
+    cin>>bin_count;
+
+    const auto bins=make_histogram(numbers, bin_count);
+
+    show_histogram_text(bins);
+}
